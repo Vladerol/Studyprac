0ea953543e514b530c5dac21e2d9b5640abb429a
Muha9208 <muhammed.gimadieff@gmail.com>
2023-04-27 14:13:31 +0300

    one

---
+++ verstka/Print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verstka
+{
+    public class Print
+    {
+        public Print(int Id,string Surname,string Name,int age,string sity)
+        {
+            this.Id = Id;
+            this.Surname = Surname;
+            this.Name = Name;
+            this.Age = age;
+            this.Sity = sity;
+        }
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Sity { get; set; }
+    }
+}
---
+++ verstka/mainKalen.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace verstka
+{
+    /// <summary>
+    /// Логика взаимодействия для mainKalen.xaml
+    /// </summary>
+    public partial class mainKalen : Page
+    {
+        public mainKalen()
+        {
+            InitializeComponent();
+           List<Print> users = new List<Print>();
+            users.Add(new Print(1, "Зайнетдинов", "Вильдан", 18, "Пекин"));
+            users.Add(new Print(2, "Чунетдинов", "Вильдус", 68, "Катар"));
+            users.Add(new Print(3, "Чубутдинов", "Чильдун", 38, "Бешкек"));
+            users.Add(new Print(4, "Pунидинов", "Вильдуни", 68, "Рим"));
+            DG.ItemsSource = users;
+        }
+        
+    }
+}
